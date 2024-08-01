6280f7ac9dd30ee1bc1b329d1bb7b50fe0bba6d2
essacx <134744380+essacx@users.noreply.github.com>
2024-08-01 06:17:14 -0400

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,141 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "The Weeknd - Die For You",       /*no borrar */
+    artist: "solo por ti",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Aiivawn - Can't Take My Eyes Off You ",
+    artist: "Quiero Abrazarte Mucho",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Chris Grey - Let The World Burn",
+    artist: "dejaria que el mundo arda",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Cigarettes After Sex - Sweet",
+    artist: "Es tan dulce saber que me amas",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "D4vd - here with me",
+    artist: "Guarda esas lagrimas",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Desde mi habitación - José y el Toro & Darviin",
+    artist: "Tu eres lo mejor de mis dias",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "MILO J - CARENCIAS DE CORDURA",
+    artist: "Me cambiaste el mundo Morcito",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "MILO J - DESEO PERDER ",
+    artist: "Yo sin vos ya no soy nada",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Elvis Presley - Can't Help Falling In Love",
+    artist: "Toma mi mano ;)",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "James Arthur - A Thousand Years ",
+    artist: "Te amare por mil años mas",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Jung Kook - Seven (Explicit Ver.)",
+    artist: "si me estoy practicando la coreo",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Jungkook - 3D (Alternate Ver.)",
+    artist: "si pudiera cruzar",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "keshi - UNDERSTAND",
+    artist: "Tu eres la unica que me entiende",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "sleeping at last - you are enough",
+    artist: "Te prometo que eres suficiente",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Lee Hi - ONLY",
+    artist: "Ti amoooo <3",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Ma Belle Evangeline)",
+    artist: "mi estrella mas bella de la creacion",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "MILO J - TU MANTA",
+    artist: "Duermo y puedo verte..",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Con Toda Libertad",             /* no borrar*/
+    artist: "Valiente ",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "MILO J - M.A.I ",
+    artist: "Muuuaaa..",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Reik - Creo En Ti",
+    artist: "Eu te amo minha princesa",
+    img: "music-20",
+    src: "music-20"
+  },
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
